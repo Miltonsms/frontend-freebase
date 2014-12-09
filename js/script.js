36ec5b8eac08831cbbd3e31a2aeaf35d07d341f4
Milton Ramirez <milsmsramirezz@gmail.com>
2014-12-09 09:56:40 -0600

    Resultado Final

--- js/script.js
+++ js/script.js
@@ -1,24 +1,164 @@
-$( document ).ready(function() {
-
-  var service_url = 'https://www.googleapis.com/freebase/v1/search';
-
-  $("#search-button").click( function() {
-    var query = $( "input[name='search']" ).val();
-    alert(query);
-
+$(document).ready(function(){
+  $('.boton').click(function(){
+    var pais = $('input:text[name=nombre]').val();
+    var service_url = 'https://www.googleapis.com/freebase/v1/search';
     var params = {
-      'query': query,
-      'filter': '(all type:/music/artist )',
-      'limit': 10,
-      'indent': true
-    };
-
-    $.getJSON(service_url + '?callback=?', params, function(response) {
-      alert(response);
-      $.each(response.result, function(i, result) {
-        $('<div>',{text:result['name']}, '</div>').appendTo(".informacion");
-      });
-    });
-  });
-
+    'query': pais,
+    'limit': 10,
+    'indent': true
+  };
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $(".mostrar").empty();
+    var conteo = 0
+    $.each(response.result,function(i, result){
+      conteo +=1
+    var obj={text:result['name']}.text;
+    var insertar='<tr>'+'<td>'+conteo+'</td>'+'<td>'+obj+'</td>'+'</tr>'
+    $('.mostrar').append(insertar);
 });
+  });
+});
+});
+/*datos de genero*/
+$(document).ready(function(){
+  $('.uno').click(function(){
+    var pais = $('input:text[name=nombre]').val();
+    var service_url = 'https://www.googleapis.com/freebase/v1/search';
+    var params = {
+    'query': pais,
+    'filter': '(all type:/music/genre)',
+    'limit': 10,
+    'indent': true
+  };
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $(".mostrar").empty();
+    var conteo = 0
+    $.each(response.result,function(i, result){
+      conteo +=1
+    var obj={text:result['name']}.text;
+    var insertar='<tr>'+'<td>'+conteo+'</td>'+'<td>'+obj+'</td>'+'</tr>'
+    $('.mostrar').append(insertar);
+});
+  });
+});
+});
+
+
+
+/* album del artista codigo*/
+$(document).ready(function(){
+  $('.dos').click(function(){
+    var pais = $('input:text[name=nombre]').val();
+    var service_url = 'https://www.googleapis.com/freebase/v1/search';
+    var params = {
+    'query': pais,
+    'filter': '(all type:/music/album)',
+    'limit': 10,
+    'indent': true
+  };
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $(".mostrar").empty();
+        var conteo = 0
+    $.each(response.result,function(i, result){
+      conteo +=1
+    var obj={text:result['name']}.text;
+    var insertar='<tr>'+'<td>'+conteo+'</td>'+'<td>'+obj+'</td>'+'</tr>'
+    $('.mostrar').append(insertar);
+    
+});
+  });
+});
+});
+/* HArea de informacion sobre libros */
+
+$(document).ready(function(){
+  $('.tres').click(function(){
+    var pais = $('input:text[name=nombre]').val();
+    var service_url = 'https://www.googleapis.com/freebase/v1/search';
+    var params = {
+    'query': pais,
+    'filter': '(all type:/book/book/)',
+    'limit': 10,
+    'indent': true
+  };
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $(".mostrar").empty();
+        var conteo = 0
+    $.each(response.result,function(i, result){
+      conteo +=1
+    var obj={text:result['name']}.text;
+    var insertar='<tr>'+'<td>'+conteo+'</td>'+'<td>'+obj+'</td>'+'</tr>'
+    $('.mostrar').append(insertar);
+    
+});
+  });
+});
+});
+
+/* harea de personas */
+$(document).ready(function(){
+  $('.tres').click(function(){
+    var pais = $('input:text[name=nombre]').val();
+    var service_url = 'https://www.googleapis.com/freebase/v1/search';
+    var params = {
+    'query': pais,
+    'filter': '(all type:/people/person)',
+    'limit': 10,
+    'indent': true
+  };
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $(".mostrar").empty();
+        var conteo = 0
+    $.each(response.result,function(i, result){
+      conteo +=1
+    var obj={text:result['name']}.text;
+    var insertar='<tr>'+'<td>'+conteo+'</td>'+'<td>'+obj+'</td>'+'</tr>'
+    $('.mostrar').append(insertar);
+    
+});
+  });
+});
+});
+/*genero de libros de la busqueda */
+$(document).ready(function(){
+  $('.libro1').click(function(){
+    var pais = $('input:text[name=nombre]').val();
+    var service_url = 'https://www.googleapis.com/freebase/v1/search';
+    var params = {
+    'query': pais,
+    'filter': '(all type:/book/book_edition)',
+    'limit': 10,
+    'indent': true
+  };
+  $.getJSON(service_url + '?callback=?', params, function(response) {
+    $(".mostrar").empty();
+        var conteo = 0
+    $.each(response.result,function(i, result){
+      conteo +=1
+    var obj={text:result['name']}.text;
+    var insertar='<tr>'+'<td>'+conteo+'</td>'+'<td>'+obj+'</td>'+'</tr>'
+    $('.mostrar').append(insertar);
+    
+});
+  });
+});
+});
+/* HAIRA QUE LIMPIA EL DIV DE INFORMACION */
+
+$(document).ready(function(){
+  $('.limpiar').click(function(){
+        $(".mostrar").empty();
+   });
+  });
+$(document).ready(function()
+    {
+    $(".cero").click(function () {  
+      $('.oculto ').toggle("slow");
+      });
+     });
+$(document).ready(function()
+    {
+    $(".libro").click(function () {  
+      $('.oculto1 ').toggle("slow");
+      });
+     });
